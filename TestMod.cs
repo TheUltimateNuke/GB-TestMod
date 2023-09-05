cbb17ad9eeb3eef0f2f7a66bf70448bf4ab7b1ec
The_UltimateNuke <84410206+TheUltimateNuke@users.noreply.github.com>
2023-09-05 08:37:58 -0400

    untitled

--- TestMod.cs
+++ TestMod.cs
@@ -1,20 +1,22 @@
-using System;
-using UnityEngine;
 using CementTools;
 
-public class TestMod : CementMod
+namespace TestMod
 {
-	public void Awake() {
-		Cement.Log(GetType().Name + " has loaded.");
-	}
+    public class TestMod : CementMod
+    {
+        public void Awake()
+        {
+            Cement.Log(GetType().Name + " has loaded.");
+        }
 
-    public void Start() 
-	{
-		Cement.Log(GetType().Name + " has started.");
-    }
-    
-    public void Update() 
-	{
-		
+        public void Start()
+        {
+            Cement.Log(GetType().Name + " has started.");
+        }
+
+        public void Update()
+        {
+
+        }
     }
 }
